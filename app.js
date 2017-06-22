91ba838843d0934af02a62e7bf98102260ebcbf2
Jeff McCord <jeff.mccord44@gmail.com>
2017-06-21 21:29:39 -0700

    oaksldjf

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 'use strict';
 
-console.log('working yet?');
+var firstImageEl = document.getElementsByClassName('first-image')[0];
+var secondImageEl = document.getElementsByClassName('second-image')[0];
+var thirdImageEl = document.getElementsByClassName('third-image')[0];
+
+function Image(name, path) {
+  this.name = name;
+  this.path = path;
+  this.shown = 0;
+  this.clicked = 0;
+}
+
+var bag = new Image('bag', './assets/bag.jpg');
+var banana = new Image('banana', './assets/banana.jpg');
+var bathroom = new Image('bathroom', './assets/bathroom.jpg');
