44a684056744def121d4b4c27632e24f78ab4322
Jeff McCord <jeff.mccord44@gmail.com>
2017-07-01 12:05:49 -0700

    added all variables

--- app.js
+++ app.js
@@ -14,6 +14,22 @@ function Image(name, path) {
 var bag = new Image('bag', './assets/bag.jpg');
 var banana = new Image('banana', './assets/banana.jpg');
 var bathroom = new Image('bathroom', './assets/bathroom.jpg');
+var boots = new Image('boots', './assets/boots.jpg');
+var breakfast = new Image('breakfast', './assets/breakfast.jpg');
+var chair = new Image('chair', './assets/chair.jpg');
+var cthulhu = new Image('cthulhu', './assets/cthulhu.jpg');
+var dogDuck = new Image('dog-duck', './assets/dog-duck.jpg');
+var dragon = new Image('dragon', './assets/dragon.jpg');
+var pen = new Image('pen', './assets/pen.jpg');
+var petSweep = new Image('pet-sweep', './assets/pet-sweep.jpg');
+var scissors = new Image('scissors', './assets/scissors.jpg');
+var shark = new Image('shark', './assets/shark.jpg');
+var sweep = new Image('sweep', './assets/sweep.png');
+var tauntaun = new Image('tauntaun', './assets/tauntaun.jpg');
+var unicorn = new Image('unicorn', './assets/unicorn.jpg');
+var usb = new Image('usb', './assets/usb.gif');
+var waterCan = new Image('water-can', './assets/water-can.jpg');
+var wineGlass = new Image('wine-glass', './assets/wine-glass.jpg');
 
 // need an event listener to track which item is clicked, then to log that choice.  And then to refresh the page with 3 new images
 console.log(Image);
